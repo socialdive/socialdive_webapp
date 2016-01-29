f985bdf8401e58cad7b107a90dd72f29cb010ed7
joshfermin <joshfermin@gmail.com>
2016-01-29 01:53:24 -0700

    Added a route that gets all photos (metadata) from DB

--- webapp/routes.js
+++ webapp/routes.js
@@ -23,9 +23,50 @@ module.exports = function(app){
 		res.render('pages/post_form');
 	});
 
-	app.post('/post_form', function(req,res){
-		// console.log(req.body, req.file);
+	// Gets all photos from the database
+	// TODO: change to get only md5 so we can use /photo/:md5 route to display.
+	app.get('/all_photos', function(req,res){
+		var Grid = require('gridfs-stream');
+		Grid.mongo = mongoose.mongo;
 
+		var conn = mongoose.createConnection(process.env.DB_CONNECT);
+		conn.once('open', function() {
+			var gfs = Grid(conn.db);
+			gfs.files.find().toArray(function (err, files) {
+				if (err) {
+					res.json(err);
+				}
+				console.log(files);
+			});
+		});
+	});
+
+	// Displays photos via their md5 
+	app.get('/photo/:md5',function(req,res){
+		var Grid = require('gridfs-stream');
+		Grid.mongo = mongoose.mongo;
+		var md5 = req.param('md5');
+
+		var conn = mongoose.createConnection(process.env.DB_CONNECT);
+		conn.once('open', function() {
+			var gfs = Grid(conn.db);
+			gfs.files.find({md5: md5}).toArray(function (err, files) {
+				if (err) {
+					res.json(err);
+				}
+				if (files.length > 0) {
+					var mime = 'image/jpeg';
+					res.set('Content-Type', mime);
+					var read_stream = gfs.createReadStream({md5: md5});
+					read_stream.pipe(res);
+				} else {
+			    res.json('File Not Found');
+				}
+			});
+		});
+	});
+
+	app.post('/post_form', function(req,res){
 		var dirname = require('path').dirname(__dirname);
 		var filename = req.file.name;
 		var path = req.file.path;
